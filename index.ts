9924ec93cc6e0b41db16ade08b1b5dacc7922599
salArsalan <ar201533@dal.ca>
2025-03-06 13:49:11 -0400

    updated index.ts

--- index.ts
+++ index.ts
@@ -1,59 +1,67 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 // Setup Pulumi config values to use later in code
 const config = new pulumi.Config();
 const dbPassword = config.requireSecret("dbPassword");
 const dbUser = config.require("dbUser");
 
-// Create a VPC with public and private subnets
-const vpc = new awsx.ec2.Vpc("salamat-a3-vpc", {
-    numberOfAvailabilityZones: 2,
+// Create a VPC
+const vpc = new aws.ec2.Vpc("salamat-a3-vpc", {
+    cidrBlock: "10.0.0.0/16",
+    enableDnsHostnames: true,
+    enableDnsSupport: true,
+    tags: { Name: "salamat-a3-vpc" },
+});
 
-    subnetSpecs: [
-        { type : "Public" },
-        { type : "Private" },
-    ],
+// Create public and private subnets in the VPC
+const publicSubnet = new aws.ec2.Subnet("public-subnet", {
+    vpcId: vpc.id,
+    cidrBlock: "10.0.1.0/24",
+    availabilityZone: "us-west-2a",
+    mapPublicIpOnLaunch: true,
+    tags: { Name: "public-subnet" },
+});
+
+const privateSubnet = new aws.ec2.Subnet("private-subnet", {
+    vpcId: vpc.id,
+    cidrBlock: "10.0.2.0/24",
+    availabilityZone: "us-west-2a",
+    tags: { Name: "private-subnet" },
 });
 
 // Create a Security Group (SG) for the EC2 instance
 const ec2sg = new aws.ec2.SecurityGroup("ec2-sg", {
-    vpcId: vpc.vpcId,
+    vpcId: vpc.id,
     description: "Allow HTTP, HTTPS, and SSH access",
-
     ingress: [
         { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
         { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
     ],
-
     egress: [{ protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] }],
 });
 
 // Create a SG for the RDS instance, allowing ONLY MySQL traffic from EC2
 const rdsSg = new aws.ec2.SecurityGroup("rds-sg", {
-    vpcId: vpc.vpcId,
+    vpcId: vpc.id,
     description: "Allow MySQL access from EC2 only",
-
     ingress: [{
         protocol: "tcp",
         fromPort: 3306,
         toPort: 3306,
         securityGroups: [ec2sg.id],
     }],
-
     egress: [{ protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] }],
 });
 
 // Create a DB Subnet Group for RDS instance using Private Subnets
 const dbSubnetGroup = new aws.rds.SubnetGroup("db-subnet-group", {
-    subnetIds: vpc.privateSubnetIds,
+    subnetIds: [privateSubnet.id],
     tags: { Name: "db-subnet-group" },
 });
 
 // Provision RDS MySQL instance
-// NOTE: Store sensitive info such as username and password in Pulumi config/secrets
 const db = new aws.rds.Instance("salamat-a3db", {
     engine: "mysql",
     instanceClass: "db.t2.micro",
@@ -61,7 +69,7 @@ const db = new aws.rds.Instance("salamat-a3db", {
     dbSubnetGroupName: dbSubnetGroup.name,
     vpcSecurityGroupIds: [rdsSg.id],
     username: dbUser,
-    password: dbPassword,       // Replace with Pulumi secret
+    password: dbPassword,
     dbName: "a3database",
     skipFinalSnapshot: true,
 });
@@ -76,7 +84,6 @@ const ami = aws.ec2.getAmi({
 });
 
 // User data script to install Docker and run specified container
-// Pull container image from Docker Hub so that the container can connect to RDS instance
 const userData = pulumi.interpolate`#!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
@@ -95,6 +102,7 @@ docker run -d -p 80:5000 \
 const ec2Instance = new aws.ec2.Instance("salamat-a3webapp-instance", {
     ami: ami.then(ami => ami.id),
     instanceType: "t2.micro",
+    subnetId: publicSubnet.id,
     vpcSecurityGroupIds: [ec2sg.id],
     userData: userData,
     associatePublicIpAddress: true,
